09bd41fa9051f3f1503e7ad65cc65fc2bed437ee
techleonic <techleonic@gmail.com>
2024-12-02 15:12:24 -0600

    this is image opening using cv2

---
+++ opencv_imageprocesing.py
@@ -0,0 +1,15 @@
+import cv2
+from matplotlib import pyplot as plt
+
+# version of cv2
+print(cv2.__version__)
+
+image = cv2.imread('img/download.jpg')
+
+# open cv uses bgr and matplolib uses rgb
+plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+plt.title("Flowers")
+plt.show()
+
+#write or save image
+cv2.imwrite("output.jpg", image)
